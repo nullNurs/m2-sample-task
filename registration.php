dc3a15d28379e9fdf968f20175fe13fc988152b1
nullNurs <91254681+nullNurs@users.noreply.github.com>
2025-11-05 17:25:00 +0500

    create module

---
+++ registration.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the Sample_CheckoutCheckbox package.
+ *
+ * @copyright Copyright (c) 2025 Nurs
+ * @author    Nurs
+ * @license   MIT
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Sample_CheckoutCheckbox',
+    __DIR__
+);
